fc9b97c8b9e00120d50a9cb38e4503283d21d584
Maksym Faizulin <maxfaizulin7@gmail.com>
2025-09-24 21:19:50 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,6 @@ import java.util.Scanner;
 class Main {
 
     public static void main(String[] args){
-        int a= 1, b = 1, c = 2, n, num;
-        System.out.println("Enter number:");
-
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();import java.util.Scanner;
-
-class Main {
-
-    public static void main(String[] args){
         int a= 1, b = 1, n, num;
         System.out.println("Enter number:");
 
@@ -40,24 +31,4 @@ class Main {
     }
 }
 
-        System.out.println("\nFibonacci number:");
-
-        for(int i = 3;i <= n; i++){
-            if (n == 0) {
-                System.out.println("0");
-                break;
-            }
-            if (n == 1 || n == 2) {
-                System.out.println("1");
-                break;
-            }
-            num = a+b;
-            a = b;
-            b = num;
-            if (i == n) {
-                System.out.println(num);
-                break;
-            }
-        }
-    }
-}
+        
