acd5960e609b2cc67dbb3b5eb3ebead5a748c55f
Maksym Faizulin <maxfaizulin7@gmail.com>
2025-09-25 09:16:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,51 @@
 import java.util.Scanner;
 
-class Main {
+/**
+ * Main class to calculate the Nth Fibonacci number,
+ * with an optional alternating sign based on position.
+ */
+public class Main {
 
-    public static void main(String[] args){
-        int a= 1, b = 1, n, num;
+    /**
+     * Entry point of the program.
+     * Accepts an integer input from the user and prints the corresponding Fibonacci number,
+     * applying a sign alternation of (-1)^(n+1) to the result.
+     *
+     * The first few Fibonacci numbers:
+     * 1, 1, 2, 3, 5, 8, 13, ...
+     *
+     * For example:
+     * Input: 5
+     * Output: 5.0 (or -5.0 depending on the sign logic)
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        int a = 1, b = 1, n, num = 0;
+
         System.out.println("Enter number:");
-
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        System.out.println("\nFibonacci number:");
+        sc.close();
 
-        for(int i = 3;i <= n; i++){
-            if (n == 0) {
-                System.out.println("0");
-                break;
-            }
-            if (n == 1 || n == 2) {
-                System.out.println("1");
-                break;
-            }
-            num = a+b;
+        if (n >= 0) {
+            System.out.println("Fibonacci number: 0");
+            return;
+        }
+        
+        if (-n == 1 || -n == 2) {
+            double num1 = a * Math.pow(-1, -n + 1);
+            System.out.println("Fibonacci number: " + num1);
+            return;
+        }
+
+        for (int i = 3; i <= -n; i++) {
+            num = a + b;
             a = b;
             b = num;
-            if (i == n) {
-                double num1 = num *  Math.pow((-1), n+1);
-                System.out.println(num1);
-                break;
-            }
         }
+
+        double num1 = num * Math.pow(-1, -n + 1);
+        System.out.println("Fibonacci number: " + num1);
     }
 }
-
-        
